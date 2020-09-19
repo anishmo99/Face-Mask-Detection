e0fa4dabdbe60c5a476de8b3f05aa35328c7c3d1
anishmo99 <ani10sh@gmail.com>
2020-09-19 15:23:23 +0530

    Update

--- detect_mask_image.py
+++ detect_mask_image.py
@@ -1,6 +1,3 @@
-# USAGE
-# python detect_mask_image.py --image images/pic1.jpeg
-
 # import the necessary packages
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
